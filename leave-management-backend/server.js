8eec815538619039a0010ca8c947799d89850e7b
Shivangi-03 <shivangi.sharma@rolaface.com>
2025-09-05 10:40:18 +0530

    frontend url chngee

--- leave-management-backend/server.js
+++ leave-management-backend/server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://rolops1.netlify.app',
   credentials: true
 }));
 app.use(bodyParser.json());
